b1fa30632c5c7e5b23bdb739fd1b54eaea7578c4
yyz <18081226d@connect.polyu.hk>
2022-12-05 08:50:44 -0800

    DSC210 Fourth Commit

--- EKF_exercise/ex6.py
+++ EKF_exercise/ex6.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Ellipse
 from math import sin, cos, atan2, sqrt
-
+import math
 
 def plot_state(mu, S, M):
     # initialize figure
@@ -16,7 +16,7 @@ def plot_state(mu, S, M):
     plt.plot(mu[0], mu[1], '.b')
     plot_2dcov(mu, S)
 
-def plot_state_2(mu, S, M):
+def plot_state_2(mu, M):
     # initialize figure
     ax = plt.gca()
     ax.set_xlim([np.min(M[:, 0]) - 2, np.max(M[:, 0]) + 2])
@@ -65,39 +65,39 @@ def plot_2d(mu):
 
     return ax.add_patch(ellipse)
 
-def wrapToPi(theta): # Ensure that theta is in the interval [-pi,pi]
-    while theta < -np.pi:
-        theta = theta + 2 * np.pi
-    while theta > np.pi:
-        theta = theta - 2 * np.pi
-    return theta
+def wrapToPi(theta): #Warp theta so that theta is always between -pi to pi
+    warp_theta=theta
+    if warp_theta < -np.pi:
+        warp_theta = warp_theta + 2 * np.pi
+    if warp_theta > np.pi:
+        warp_theta = warp_theta - 2 * np.pi
+    return warp_theta
+
+def inv_motion_model(u_t): #Decompose translation into rotation 1， rotation 2 and translation
+    translation = sqrt((u_t[1][0] - u_t[0][0]) ** 2 + (u_t[1][1] - u_t[0][1]) ** 2)
+    rotation_1 = wrapToPi(atan2((u_t[1][1] - u_t[0][1]), (u_t[1][0] - u_t[0][0])) - u_t[0][2])
+    rotation_2 = wrapToPi(u_t[1][2] - u_t[0][2] - rotation_1)
+
+    return rotation_1, translation, rotation_2
 
 
-def inv_motion_model(u_t): #Translated command u_t to translation and rotation
-    trans = sqrt((u_t[1][0] - u_t[0][0]) ** 2 + (u_t[1][1] - u_t[0][1]) ** 2)
-    rot1 = wrapToPi(atan2((u_t[1][1] - u_t[0][1]), (u_t[1][0] - u_t[0][0])) - u_t[0][2])
-    rot2 = wrapToPi(u_t[1][2] - u_t[0][2] - rot1)
-
-    return rot1, trans, rot2
-
-
-def ekf_predict(mu, sigma, u_t, R): # Predict next state from current state and commands u_t
+def ekf_predict(mu, sigma, u_t, R): #Predict mu and sigma through u_t and R
     theta = mu[2][0]
 
-    rot1, trans, rot2 = inv_motion_model(u_t)
+    rotation_1, translation, rotation_2 = inv_motion_model(u_t)
 
-    G_t = np.array([[1, 0, -trans * sin(theta + rot1)],
-                    [0, 1, trans * cos(theta + rot1)],
+    G_t = np.array([[1, 0, -translation * sin(theta + rotation_1)],
+                    [0, 1, translation * cos(theta + rotation_1)],
                     [0, 0, 1]])
 
-    V_t = np.array([[-trans * sin(theta + rot1), cos(theta + rot1), 0],
-                    [trans * cos(theta + rot1), sin(theta + rot1), 0],
+    V_t = np.array([[-translation * sin(theta + rotation_1), cos(theta + rotation_1), 0],
+                    [translation * cos(theta + rotation_1), sin(theta + rotation_1), 0],
                     [1, 0, 1]])
-    mu_bar = mu + np.array([[trans * cos(theta + rot1)],
-                            [trans * sin(theta + rot1)],
-                            [rot1 + rot2]])              #Predict the robot' state
+    mu_bar = mu + np.array([[translation * cos(theta + rotation_1)],
+                            [translation * sin(theta + rotation_1)],
+                            [rotation_1 + rotation_2]])
 
-    sigma_bar = G_t @ (sigma @ G_t.T) + V_t @ (R @ V_t.T)
+    sigma_bar = np.matmul(G_t ,np.matmul (sigma , G_t.T)) + np.matmul(V_t ,np.matmul(R , V_t.T))
 
     return mu_bar, sigma_bar
 
@@ -108,25 +108,25 @@ def ekf_correct(mu_bar, sigma_bar, z, Q, M):
         lx = M[j, 0]
         ly = M[j, 1]
 
-        q = (lx - mu_bar[0, 0]) ** 2 + (ly - mu_bar[1, 0]) ** 2
-        dist = sqrt(q)
+        q = (lx - mu_bar[0, 0]) *(lx - mu_bar[0, 0]) + (ly - mu_bar[1, 0]) *(ly - mu_bar[1, 0])
+        dist_zi = sqrt(q)
 
         # wrap to pi as the angle must be between -pi to pi everywhere we deal with the angle
 
-        z_hat = np.array([[dist],
+        z_i = np.array([[dist_zi],
                           [wrapToPi(atan2(ly - mu_bar[1, 0], lx - mu_bar[0, 0]) - mu_bar[2, 0])]])
 
         H_t = np.array(
-            [[-(lx - mu_bar[0, 0]) / dist, -(ly - mu_bar[1, 0]) / dist, 0],
+            [[-(lx - mu_bar[0, 0]) / dist_zi, -(ly - mu_bar[1, 0]) / dist_zi, 0],
              [(ly - mu_bar[1, 0]) / q, -(lx - mu_bar[0, 0]) / q, -1]])
 
-        S = H_t @ sigma_bar @ H_t.T + Q
+        S_t = np.matmul(H_t ,np.matmul( sigma_bar , H_t.T)) + Q
 
-        K = sigma_bar @ H_t.T @ np.linalg.inv(S) #compute the Kalman Gain and the most time-consuming step: inversion!
+        K_t = np.matmul(sigma_bar ,np.matmul( H_t.T , np.linalg.inv(S_t))) #compute the Kalman Gain and the most time-consuming step: inversion!
 
-        mu_bar = mu_bar + (K @ (z[:2, i].reshape(2, 1) - z_hat)) #The correct step
+        mu_bar = mu_bar + (np.matmul(K_t , (z[:2, i].reshape(2, 1) - z_i))) #The correct step
         mu_bar[2, 0] = wrapToPi(mu_bar[2, 0])
-        sigma_bar = (np.identity(3) - (K @ H_t)) @ sigma_bar #Reduce the uncertainty
+        sigma_bar = np.matmul((np.identity(3) - np.matmul(K_t , H_t)) , sigma_bar) #Reduce the uncertainty
 
     return mu_bar, sigma_bar
 
